ae5e2abf7de308e3371e5470be17265ad1005445
Shubhang Sharma <shubsh01@gmail.com>
2025-04-25 07:25:20 +0530

    fetch nav nd footer

---
+++ script.js
@@ -0,0 +1,14 @@
+window.addEventListener('DOMContentLoaded', () => {
+    fetch('partials/nav.html')
+      .then(res => res.text())
+      .then(data => {
+        document.getElementById('navbar').innerHTML = data;
+      });
+  
+    fetch('partials/footer.html')
+      .then(res => res.text())
+      .then(data => {
+        document.getElementById('footer').innerHTML = data;
+      });
+  });
+  
